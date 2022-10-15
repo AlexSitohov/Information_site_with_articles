13b53804a60add4b7b53653a5e28dfc6793f3586
AlexSitohov <lilinfinitemob@gmail.com>
2022-10-16 00:05:42 +0300

    upd 1.9.3 -- ready_to_deploy

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
